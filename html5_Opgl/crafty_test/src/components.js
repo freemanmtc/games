ed8fae387bd7ca4d229b6c8e7c9772689cab56fc
U-icomachine\ico <ico@icomachine.(none)>
2013-08-07 00:09:15 -0300

    map crated, the random bushes are working, fixed some syntax errors

--- html5_Opgl/crafty_test/src/components.js
+++ html5_Opgl/crafty_test/src/components.js
@@ -1,32 +1,43 @@
-Crafty.c('Grid', {
-	init: function() {
+/* o componente grid abilita um elemento a ser localizado */
+/* um grid de tiles */
+Crafty.c('Grid',{
+	init: function(){
 		this.attr({
-			w: Game.map_grid.tile.width,
-			h: Game.map_grid.tile.height
+			w:Game.map_grid.tile.width,
+			h:Game.map_grid.tile.height
 		})
 	},
- 
-	// Locate this entity at the given position on the grid
-	at: function(x, y) {
-		if (x === undefined && y === undefined) {
-			return { x: this.x/Game.map_grid.tile.width, y: this.y/Game.map_grid.tile.height }
-		} else {
-			this.attr({ x: x * Game.map_grid.tile.width, y: y * Game.map_grid.tile.height });
+
+
+	/* localiza a entidade ma posisao dada no grid */
+	at: function(x,y){
+		if(x===undefined && y===undefined){
+			return{x:this.x/Game.map_grid.tile.width,y:this.y/game.map_grid.tile.height}
+		}else{
+			this.attr({x: x * Game.map_grid.tile.width,y: y * Game.map_grid.tile.height});
 			return this;
-		 }
+		}
 	}
 });
-  
-Crafty.c('Tree', {
+
+/* ator */
+Crafty.c('Actor',{
 	init: function() {
-		this.requires('2D, Canvas, Grid, Color');
-		this.color('rgb(20, 125, 40)');
+		this.requires('2D,Canvas,Grid');
+	}
+});
+/* arvore */
+Crafty.c('Tree',{
+	init: function(){
+		this.requires('Actor,Color')
+		.color('rgb(20,125,40)');
 	},
 });
-   
-Crafty.c('Bush', {
-	init: function() {
-		this.requires('2D, Canvas, Grid, Color');
-		this.color('rgb(20, 185, 40)');
+
+/* arbusto */
+Crafty.c('Bush',{
+	init: function(){
+		this.requires('Actor,Color')
+		.color('rgb(20.185.40)');
 	},
 });
--- html5_Opgl/crafty_test/src/game.js
+++ html5_Opgl/crafty_test/src/game.js
@@ -1,74 +1,44 @@
 Game = {
-	/*
-	/* Initialize and start our game */
-	start: function()
-	{
-		/* Start crafty and set a background color so that we can see it's working */
-		Crafty.init(480, 320);
-		Crafty.background('#cccccc');
-	}
-	*/
-	
 	/* define o tamanho do grid e o tamanho de cada tile */
-	map_grid:
-	{
-		width:24,
-		height:16,
+	map_grid: {
+		width: 24,
+		height: 16,
 		
-		tile{
-			width:16,
-			height:16
+		tile: {
+			width: 16,
+			height: 16
 		}
 	},
 
 	/* a largura total da tela do jogo, ja que o grid se extende pela tela inteira */
 	/* largura de um tile veses a largura do grid */
-	width: function()
-	{
-		return this.mad_grid.width * this.map_grid.tile.width;
+	width: function() {
+		return this.map_grid.width * this.map_grid.tile.width;
 	},
 
 	/* a altura total da tela do jogo, ja que o grid se extende pela tela inteira */	
 	/* altura de um tile veses a largura do grid */
-	height: function()
-	{
+	height: function() {
 		return this.map_grid.height * this.map_grid.tile.height;
 	},
 
 	/* inicializa o crafty e inicia o jogo */
-	start: function()
-	{
+	start: function() {
 		/* inicia o craty e seta o background */
-		Crafty.init(Game.width(),game.height());
+		Crafty.init(Game.width(),Game.height());
 		Crafty.background('rgb(249,223,125)');
 		
 		/* coloca uma arvore em cada canto do viewport */
-		for(var x=0;x<Game.map_grid.width;x++)
-		{
-			for(var y=0;y<map_grid.height;y++)
-			{
-				var at_edge=x== 0 || x==Game.map_grid.width -1 || y==0 || y==Game.map_grid.height -1;
+		for(var x=0; x < Game.map_grid.width; x++) {
+			for(var y=0; y < Game.map_grid.height; y++) {
+				var at_edge = x == 0 || x == Game.map_grid.width -1 || y == 0 || y == Game.map_grid.height -1;
 
-				/* coloca a ientidade arvore no tile atual */
-				if(at_edge)
-				{
-					Crafty.e('2D,Canvas,Color').attr({
-						x: x * Game.map_grid.tile.width,
-						y: y * game.map_grid.tile.height,
-						w: Game.map_grid.tile.width,
-						h: Game.map_grid.tile.height	
-					}).color('rgb(20,125,40)');
-				}
-				elseif(Math.random() < 0.06)
-				{
+				if(at_edge) {
 					/* coloca a ientidade arvore no tile atual */
-					Crafty.e('2D,Canvas,Color').attr({
-						x: x * Game.map_grid.tile.width,
-						y: y * game.map_grid.tile.height,
-						w: Game.map_grid.tile.width,
-						h: Game.map_grid.tile.height	
-					}).color('20,125,40');
-
+					Crafty.e('Tree').at(x,y);
+				} else if (Math.random() < 0.06) {
+					/* coloca a ientidade arbusto no tile atual */
+					Crafty.e('Bush').at(x,y);
 				}
 			}
 		}
